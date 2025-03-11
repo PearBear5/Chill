d89283e1e6e6efa0cf6e83f60c06ae7791a3442a
PearBear5 <137337886+PearBear5@users.noreply.github.com>
2025-03-11 10:29:58 -0700

    ducks

--- js/interactive.js
+++ js/interactive.js
@@ -1,9 +1,28 @@
 const duck = document.querySelector('.duck')
 const count = document.querySelector('.count')
+const bread = document.querySelector('.bread')
 
 let amount = 0
+let bram = 0
 
 duck.addEventListener('click', ()=>{
     amount = amount + 1
     count.innerText = `Ducks:${amount}`
 })
+
+duck.addEventListener('click', ()=>{
+    if (amount > 9)
+    {
+        amount = amount - 10
+        bram = bram + 1
+        bread.innerText = `Costs 10 Ducks: ${bram}`
+    }
+})
+
+let int = setInterval(add, 3)
+
+function add() {
+    amount = amount + bram
+}
+
+
