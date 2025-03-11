eeae6312c846d1e65716b34824550e5d5bfcf7bc
PearBear5 <137337886+PearBear5@users.noreply.github.com>
2025-03-11 16:46:26 +0000

    Tue, Mar 11, 2025, 9:46 AM -07:00

--- js/interactive.js
+++ js/interactive.js
@@ -1,1 +1,9 @@
 const duck = document.querySelector('.duck')
+const count = document.querySelector('count')
+
+let amount = 0
+
+duck.addEventListener('click', ()=>{
+    amount = amount + 1
+    count.innerText = `Ducks:${amount}`
+})
