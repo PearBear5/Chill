7f1edee727be51d0c621291e6a7000029a397e0c
PearBear5 <137337886+PearBear5@users.noreply.github.com>
2025-03-31 17:07:34 +0000

    Mon, Mar 31, 2025, 10:07 AM -07:00

--- js/relax.js
+++ js/relax.js
@@ -5,8 +5,8 @@ setInterval(breathAnimation(), 7500)
 
 function breathAnimation() {
     text.innerHTML = 'Breath In'
-    container.classList.remove(shrink)
-    container.classList.add(grow)
+    container.classList.remove('shrink')
+    container.classList.add('grow')
 
     setTimeout( () => {
         text.innerHTML = 'Hold'
@@ -14,7 +14,7 @@ function breathAnimation() {
 
     setTimeout( () => {
         text.innerHTML = 'Breath Out'
-        container.classList.remove(grow)
-        container.classList.add(shrink)
+        container.classList.remove('grow')
+        container.classList.add('shrink')
     }, 4500)
 }
