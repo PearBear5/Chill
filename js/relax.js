df91e2cf40fb787cfd477222d84645ade9e8e11d
PearBear5 <137337886+PearBear5@users.noreply.github.com>
2025-03-31 16:54:59 +0000

    Mon, Mar 31, 2025, 9:54 AM -07:00

--- js/relax.js
+++ js/relax.js
@@ -1,1 +1,8 @@
-const container
+const container = document.getElementById('container')
+const text = document.getElementById('text')
+
+function breathAnimation() {
+    text.innerHTML = 'Breath In'
+    container.classlist.add(grow)
+    
+}
